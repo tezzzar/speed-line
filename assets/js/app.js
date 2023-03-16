9d64edd76469220b591ebf148aadbc33cb3f03ad
tezzzar <olexandr.borodavko@gmail.com>
2023-03-16 16:35:31 +0200

    fix search_dropdown_menu

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+// $(document).ready(function () {
+//   $(".owl-carousel").owlCarousel();
+// });
+
+function toggleDropdown() {
+  var dropdown = document.getElementById("dropdown");
+  if (dropdown.style.display === "block") {
+    dropdown.style.display = "none";
+  } else {
+    dropdown.style.display = "block";
+  }
+}
