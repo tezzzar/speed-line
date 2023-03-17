916851baa37afe4a49686fa0828c44975da75552
tezzzar <olexandr.borodavko@gmail.com>
2023-03-17 02:33:54 +0200

    add fonts

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,38 @@
-// $(document).ready(function () {
-//   $(".owl-carousel").owlCarousel();
-// });
+// DROPDOWN-MENU -----------------------------------
+
+var dropdown = document.getElementById("dropdown");
+var toggle = document.getElementById("toggle");
 
 function toggleDropdown() {
-  var dropdown = document.getElementById("dropdown");
   if (dropdown.style.display === "block") {
     dropdown.style.display = "none";
   } else {
     dropdown.style.display = "block";
   }
 }
+document.addEventListener("click", function (event) {
+  if (!toggle.contains(event.target)) {
+    dropdown.style.display = "none";
+  }
+});
+
+// SLIDER -------------------------------------------
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    nav: true,
+    dots: true,
+    loop: true,
+    animateOut: "fadeOut",
+    // navText: ["", ""],
+  });
+});
+
+$(".next_button").click(function () {
+  owl.trigger("next.owl.carousel");
+});
+
+$(".prev_button").click(function () {
+  owl.trigger("prev.owl.carousel");
+});
