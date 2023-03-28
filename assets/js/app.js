daa6668afe5de91b3597c70b11f61342a5dff16e
tezzzar <olexandr.borodavko@gmail.com>
2023-03-28 17:09:06 +0300

    add responsive header & footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,7 @@
-// DROPDOWN-MENU -----------------------------------
-
-var dropdown = document.getElementById("dropdown");
-var toggle = document.getElementById("toggle");
-
-function toggleDropdown() {
-  if (dropdown.style.display === "block") {
-    dropdown.style.display = "none";
-  } else {
-    dropdown.style.display = "block";
-  }
-}
-document.addEventListener("click", function (event) {
-  if (!toggle.contains(event.target)) {
-    dropdown.style.display = "none";
-  }
-});
-
 // SLIDER -------------------------------------------
 
 $(document).ready(function () {
-  $("#slider-carousel").owlCarousel({
+  $(".slider-carousel").owlCarousel({
     items: 1,
     nav: true,
     dots: true,
@@ -27,10 +9,8 @@ $(document).ready(function () {
     loop: true,
     video: true,
     lazyLoad: true,
-    // center: true,
-    // autoWidth: true,
+    pause: "hover",
     animateOut: "fadeOut",
-
     // autoplay: true,
     // autoplayTimeout: 7500, //Время смены слайда
     // onInitialized: function () {
@@ -52,27 +32,92 @@ $(document).ready(function () {
     //   }
     // },
   });
+
+  // $(window).on("load", function () {
   $(".catalog-carousel").owlCarousel({
     items: 5,
     loop: true, //Зацикливаем слайдер
     margin: 15, //Отступ от картино если выводите больше 1
-    nav: true,
-    dots: true,
+    // nav: true,
+    // dots: true,
     // autoplay: true, //Автозапуск слайдера
     // smartSpeed: 2000, //Время движения слайда
     // autoplayTimeout: 4000, //Время смены слайда
-    // responsive: {
-    //   0: {
-    //     items: 1,
-    //   },
-    //   600: {
-    //     items: 2,
-    //   },
-    //   1000: {
-    //     items: 5,
-    //   },
-    // },
+    // responsiveClass: true,
+    // responsiveBaseElement: "main",
+    responsive: {
+      0: {
+        items: 2,
+        nav: true,
+        dots: false,
+      },
+      768: {
+        items: 3,
+        nav: true,
+        dots: true,
+      },
+      1024: {
+        items: 4,
+        nav: true,
+        dots: true,
+        // margin: 25,
+      },
+      1440: {
+        items: 5,
+        nav: true,
+        dots: true,
+      },
+    },
   });
+  // });
+});
+
+// $(document).ready(function () {
+//   $(".catalog-carousel").owlCarousel({
+//     loop: true, //включаем бесконечную прокрутку
+//     nav: true, // пишем это вместо "navigation" и включаем кнопки 'prev' и 'next'
+//     navText: [
+//       "<i class='fa fa-chevron-left'></i>",
+//       "<i class='fa fa-chevron-right'></i>",
+//     ], // пишем это вместо "navigationText" и подключаем свои кнопки
+//     dots: false, // вместо "pagination", отключаем точки
+//     autoplay: true, // включаем автопрокрутку
+//     autoplaySpeed: 500, // пишем это вместо "slideSpeed" и задаем скорость прокрутки
+//     responsiveClass: true,
+//     center: true,
+//     responsive: {
+//       0: {
+//         items: 0, // от 0px до 599px слайдов нет
+//       },
+//       600: {
+//         items: 1, // от 600px до 899px один слайд
+//       },
+//       900: {
+//         items: 2, // от 900px до 1199px два слайда
+//       },
+//       1200: {
+//         items: 3, // от 1200px и больше три слайда
+//       },
+//     },
+//   });
+// });
+
+// DROPDOWN-MENU -----------------------------------
+
+var dropdown = document.getElementById("dropdown");
+var toggle = document.getElementById("toggle");
+
+function toggleDropdown() {
+  if (dropdown.style.display === "block") {
+    dropdown.style.display = "none";
+  } else {
+    dropdown.style.display = "block";
+  }
+}
+document.addEventListener("click", function (event) {
+  if (!toggle.contains(event.target)) {
+    dropdown.style.display = "none";
+  }
 });
 
 // Burger menu --------------------------------------------
@@ -96,3 +141,30 @@ $(document).ready(function () {
     $(this).children(burgerSubMenu).slideToggle();
   });
 });
+
+// --------------Button Up---------------------------
+
+const scrollTrigger = 1.5;
+const btnUp = {
+  el: document.querySelector(".button-top"),
+  show() {
+    this.el.classList.remove("btn-up_hide");
+  },
+  hide() {
+    this.el.classList.add("btn-up_hide");
+  },
+  addEventListener() {
+    window.addEventListener("scroll", () => {
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      scrollY > window.innerHeight * scrollTrigger ? this.show() : this.hide();
+    });
+    document.querySelector(".button-top").onclick = () => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    };
+  },
+};
+btnUp.addEventListener();
